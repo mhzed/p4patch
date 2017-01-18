3288e69dd1358e227028917d21b6dd56eb572f3d
mhzed <minhongz@gmail.com>
2017-01-18 14:42:31 +0800

    first

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 p4patch
 --------
 
+Generate patch via "p4" command line.  "p4 diff" ignores newly added files, this
+utility includes added files in the patch.
+
+Ensure that 'p4' can run successfully in the terminal, then go to the local p4v check out 
+folder, just run:
+
+    p4patch
+    
+or
+    
+    p4patch > patch.txt
+    
+    
+    
--- p4patch.js
+++ p4patch.js
@@ -1,81 +1,75 @@
 #!/usr/bin/env node
-
-const co = require("co");
-const pmfy = require("pmfy");
-const child_process = require("child_process");
-const fs = require("fs");
-const process = require("process");
-const log = require("lawg");
-const _ = require("lodash");
+const co = require('co')
+const pmfy = require('pmfy')
+const childProcess = require('child_process')
+const fs = require('fs')
+const _ = require('lodash')
 /**
  * Run in the local dir under p4v control, patch is generated for all files checked out under this
  * local dir.
  *
  */
-OutFileName = "p4patch.txt";
 const pm = {
-  process : pmfy(process),
-  child_process : pmfy(child_process)
+  process: pmfy(process),
+  childProcess: pmfy(childProcess)
 }
-const exec = pm.child_process.exec;
+const exec = pm.childProcess.exec
 
 co(function*() {
-  "use strict";
+  'use strict'
 
-  const pwd = process.cwd();
+  const pwd = process.cwd()
 
-  yield exec(`p4 diff -dcu ./... > ${OutFileName}`);
+  let [diffstdout] = yield exec(`p4 diff -dcu ./...`)
+  console.log(diffstdout)
 
   // figure out new files
-  let newFiles;
+  let newFiles
   {
-    let nOverlap = 0;
-    const [stdout] = yield exec(`p4 revert -n ./... | grep 'was add'`);
-    newFiles = _(stdout.split("\n")).map(
-      (l)=>l.split(/\s/)[0].replace(/\#none$/,'')
-    ).filter().map(
-      (depotFile)=>{
-        let localFile = "";
+    let nOverlap = 0
+    const [stdout] = yield exec(`p4 revert -n ./...`)
+    newFiles = _(stdout.split('\n'))
+      .filter((l) => /was added/.test(l))
+      .map((l) => l.split(/\s/)[0].replace(/#none$/, ''))
+      .filter()
+      .map((depotFile) => {
+        let localFile
         if (!nOverlap) {
-          const lastLocalTok = pwd.split(/[\\\/]/).pop();
-          nOverlap = depotFile.indexOf(`/${lastLocalTok}`) + lastLocalTok.length + 1;
+          const lastLocalTok = pwd.split(/[\\/]/).pop()
+          nOverlap = depotFile.indexOf(`/${lastLocalTok}`) + lastLocalTok.length + 1
         }
-        localFile = pwd + depotFile.slice(nOverlap);
+        localFile = pwd + depotFile.slice(nOverlap)
         return {
           depot: depotFile,
           local: localFile
         }
       }
-    ).value();
+    ).value()
   }
-  const countline = (str)=>{
-    let i = -1, n=0;
+  const countline = (str) => {
+    let i, n = 0
     while (true) {
-      i = str.indexOf("\n");
-      if (i!=-1) { str = str.slice(i+1); n++;}
-      else break;
+      i = str.indexOf('\n')
+      if (i !== -1) { str = str.slice(i + 1); n++ } else break
     }
-    return n;
+    return n
   }
+
   // append new files to patch
   for (let {depot, local} of newFiles) {
-    //log(lf);
-    let content = fs.readFileSync(local).toString();
-    let nline = countline(content);
-    content = _(content.split("\n")).map((l)=>'+'+l).join("\n");
+    let content = fs.readFileSync(local).toString()
+    let nline = countline(content)
+    content = _(content.split('\n')).map((l) => '+' + l).join('\n')
     let patchStr = `
 ==== ${depot}#1 - ${local} ====
 @@ -1,1 +1,${nline} @@
 ${content}
-    `;
-    fs.appendFileSync(OutFileName, patchStr);
+    `
+    console.log(patchStr)
   }
-
-
-}).then(()=>{
-  "use strict";
-  console.log(OutFileName + " is generated");
-}).catch((err)=>{
-  console.error(err);
-  process.exit(1);
-});
+}).then(() => {
+  'use strict'
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
